59e8c54343634d9609d0c223945af1e9293381ac
Erin Ponsonby <erinponsonby@gmail.com>
2019-10-08 13:22:27 -0400

    Add Shops and Users migration

---
+++ db/migrate/20191008171241_create_shops.rb
@@ -0,0 +1,13 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :neighborhood
+      t.string :seating
+      t.string :noise_level
+      t.string :food
+      t.boolean :wifi
+      t.boolean :outlets
+    end
+  end
+end
